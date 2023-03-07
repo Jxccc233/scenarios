abe0089ea34d022ef50cc64cd6effb0f3f200714
Jxccc233 <1452038784@qq.com>
2023-03-07 18:00:19 +0800

    fix test

--- numpy/challenge-linear-algebra-solving-with-numpy/assets/determinant_matrix_test.py
+++ numpy/challenge-linear-algebra-solving-with-numpy/assets/determinant_matrix_test.py
@@ -10,9 +10,9 @@ from determinant_matrix import determinant_matrix
 class TestMatrixInversion(unittest.TestCase):
     def test_determinant_matrix(self):
         A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-        B = np.array([[1, 2], [3, 4]])
+        B = np.array([[1, 0, 0], [0, 2, 0], [0, 0, 3]])
         self.assertEqual(determinant_matrix(A), -0.0)
-        self.assertEqual(determinant_matrix(B), -2.0000000000000004)
+        self.assertEqual(determinant_matrix(B), 6.0)
 
 if __name__ == '__main__':
     unittest.main()
