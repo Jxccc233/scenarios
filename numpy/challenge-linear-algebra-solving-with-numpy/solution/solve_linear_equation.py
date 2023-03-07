c03cad86a5f2c031990dd7f7bc676e8c544e5b7b
Jxccc233 <1452038784@qq.com>
2023-03-07 16:11:33 +0800

    add-challenge-linear-algebra-solving-with-numpy

---
+++ numpy/challenge-linear-algebra-solving-with-numpy/solution/solve_linear_equation.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def solve_linear_equation(A: np.ndarray, b: np.ndarray) -> np.ndarray:
+    """
+    Solves the linear equation Ax = b using matrix inversion.
+    If the matrix A is not invertible or the dimensions of A and b do not match,
+    raises a ValueError with an appropriate error message.
+
+    Args:
+        A: A NumPy matrix representing the coefficients of the linear equation.
+        b: A NumPy matrix representing the constants of the linear equation.
+
+    Returns:
+        The solution to the linear equation Ax = b as a NumPy array.
+
+    Raises:
+        ValueError: If the matrix A is not invertible or the dimensions of A and b do not match.
+    """
+    if A.shape[0] != A.shape[1] or A.shape[0] != b.shape[0]:
+        raise ValueError("Matrix A and matrix b dimensions do not match")
+    inv_A = invert_matrix(A)
+    return np.matmul(inv_A, b)
---
+++ numpy/challenge-linear-algebra-solving-with-numpy/step5.md
@@ -0,0 +1,23 @@
+## Sub-Challenge: Eigenvector and Eigenvalue Calculation
+
+### TODO
+
+Write a Python function `solve_linear_equation(matrix)` that takes a NumPy matrix as input and calculates its eigenvectors and eigenvalues. The function should return two NumPy arrays: one containing the eigenvectors and another containing the corresponding eigenvalues.
+
+### Example Input
+
+```python
+import numpy as np
+
+A = np.array([[1, 2],
+              [2, 1]])
+
+eigenvector_eigenvalue(A)
+```
+
+### Example Output
+
+```lua
+(array([[ 0.70710678, -0.70710678],
+        [ 0.70710678,  0.70710678]]), array([ 3., -1.]))
+```
